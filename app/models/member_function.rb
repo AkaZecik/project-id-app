9ed59f7eaf9068aeedb13cdc953a70340b55c4bc
Artur Zubilewicz <artur.zubilewicz@gmail.com>
2018-06-17 16:12:36 +0200

    Change name of associations

--- app/models/member_function.rb
+++ app/models/member_function.rb
@@ -1,5 +1,5 @@
 class MemberFunction < ApplicationRecord
-  belongs_to :function, foreign_key: :function
+  belongs_to :the_function, class_name: "Function", foreign_key: :function
   belongs_to :member
   validates :member_id, uniqueness: {scope: [:function, :since]}
 end
--- app/models/radio_performance.rb
+++ app/models/radio_performance.rb
@@ -1,5 +1,5 @@
 class RadioPerformance < ApplicationRecord
-  belongs_to :radio, foreign_key: :radio
+  belongs_to :the_radio, class_name: "Radio", foreign_key: :radio
   belongs_to :performance
   validates :performance_id, uniqueness: {scope: [:radio, :time]}
 end
