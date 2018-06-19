227641ddd301a5b5f7455df1104d29633aba8d34
Artur Zubilewicz <artur.zubilewicz@gmail.com>
2018-06-19 14:30:28 +0200

    Disallow artists from featuring themselves

--- app/models/featuring.rb
+++ app/models/featuring.rb
@@ -3,4 +3,14 @@ class Featuring < ApplicationRecord
   belongs_to :song
   validates :artist_id, uniqueness: {scope: :song_id}
   validates :artist_id, :song_id, presence: true, numericality: true
+  validate :self_featuring
+
+  private
+
+  def self_featuring
+    if self.artist_id == Song.find_by(artist_id: self.artist_id)&.artist_id
+      errors[:base] << "Artists cannot feature themselves"
+      throw :abort
+    end
+  end
 end
